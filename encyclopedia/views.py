1ecdab24ba556fed4876ea0faf4aa3f01fcd1510
poyrazn <poyrazf@mef.edu.tr>
2020-11-15 19:18:36 +0300

    entry method added to return searched entry title

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from markdown2 import Markdown
 from . import util
 
 
@@ -7,4 +7,17 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+
+def entry(request, title):
+    try:
+        content = Markdown().convert(util.get_entry(title))
+    except TypeError:
+        return render(request, "encyclopedia/error.html", status=404)
+
+    return render(request, "encyclopedia/entry.html", {
+        "title": title.capitalize(),
+        "entry": content
+    })
+
 
