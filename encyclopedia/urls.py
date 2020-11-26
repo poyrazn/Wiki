510a62e3c6f93a5652dd6d7c17e747402d345358
poyrazn <poyrazf@mef.edu.tr>
2020-11-26 23:21:29 +0300

    changed url paths

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry"),
-    path("create", views.create, name="create"),
-    path("random", views.random_page, name="random"),
-    path("edit", views.edit, name="edit")
+    path("wiki/<str:title>", views.entry, name="entry"),
+    path("wiki/create/", views.create, name="create"),
+    path("wiki/random/", views.random_page, name="random"),
+    path("wiki/edit/", views.edit, name="edit")
 ]
